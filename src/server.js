0c0ef3f7cd3710ddb506cc7f9ec0527d91d409c9
b4yerl <gcbayerlc@gmail.com>
2023-02-08 07:48:01 -0300

    Initial server structure
    Server is able to run either in development or production mode.
    Initial dependencies installed.
    Unhandled Rejections now has a handler.

---
+++ src/server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const colors = require('colors');
+const morgan = require('morgan');
+
+dotenv.config({ path: './config/config.env' });
+
+const app = express();
+
+// Using logger only while on development
+if(process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
+const port = process.env.PORT || 5000;
+const server = app.listen(port, () => console.log(`Server running in ${process.env.NODE_ENV} mode on port ${port}`.green.inverse));
+
+// A way to handle unhandled rejections :)
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`Error: ${err.message}`.red);
+  server.close(() => process.exit(1));
+})
