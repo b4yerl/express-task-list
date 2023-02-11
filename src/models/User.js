5d414699e6ad0961535e0e3cab6339fc91fd19e4
b4yerl <gcbayerlc@gmail.com>
2023-02-11 09:43:51 -0300

    Add User model and password hashing feature
    Now users can be registered and searched through the database.
    Password hashing and comparing using bcryptjs

---
+++ src/models/User.js
@@ -0,0 +1,54 @@
+const mongoose = require('mongoose');
+const bcryptjs = require('bcryptjs');
+
+const UserSchema = mongoose.Schema({
+  username: {
+    type: String,
+    required: [true, 'Please provide a username'],
+    maxlength: [30, 'Username must have less than 30 characters']
+  },
+  email: {
+    type: String,
+    required: [true, 'Please add an email'],
+    unique: true,
+    match: [
+      /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+      'Please add a valid email'
+    ]
+  },
+  role: {
+    type: String,
+    enum: ['user'],
+    default: 'user'
+  },
+  password: {
+    type: String,
+    required: [true, 'Please add a password'],
+    minlength: [6, 'Password must have at least 6 characters'],
+    // match: a-z A-Z 0-9 !-?
+  },
+  resetPasswordToken: String,
+  resetPasswordExpire: Date,
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+// Hash password using bcryptjs
+UserSchema.pre('save', async function(next) {
+  // Prevent password hashing every time other user info is updated
+  if(!this.isModified('password')) next();
+
+  const salt = await bcryptjs.genSalt();
+  this.password = await bcryptjs.hash(this.password, salt);
+
+  next();
+});
+
+// Match input password to the database one
+UserSchema.methods.matchPasswords = async function(input) {
+  return await bcryptjs.compare(input, this.password);
+};
+
+module.exports = mongoose.model('User', UserSchema);
