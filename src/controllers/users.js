2d8534ad0a4f6346dd29248330649e0999ef17bb
b4yerl <gcbayerlc@gmail.com>
2023-02-14 09:27:08 -0300

    Add endpoint for admins to delete users

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -32,6 +32,7 @@ exports.getSingleUser = asyncHandler(async (req, res, next) => {
 // @routes PATCH /api/v1/users/:id
 // @access Private (admin)
 exports.updateUser = asyncHandler(async (req, res, next) => {
+  // Don't allow admin to update user's password
   const { email, username } = req.body;
   const user = await User.findByIdAndUpdate(req.params.id, { email, username}, {
     new: true,
@@ -39,6 +40,20 @@ exports.updateUser = asyncHandler(async (req, res, next) => {
   });
 
   if(!user) return next(new ErrorResponse('User not found', 404));
+  
+  res.status(200).json({ success: true, data: user });
+});
+
+// @desc   Delete user
+// @routes DELETE /api/v1/users/:id
+// @access Private (admin)
+exports.deleteUser = asyncHandler(async (req, res, next) => {
+  const user = await User.findById(req.params.id);
+  
+  // Verify user existence
+  if(!user) return next(new ErrorResponse('User not found', 404));
+
+  await User.findByIdAndDelete(req.params.id);
   
   res.status(200).json({ success: true, data: user });
 });
--- src/routes/users.js
+++ src/routes/users.js
@@ -2,7 +2,8 @@ const express = require('express');
 const {
   getAllUsers,
   getSingleUser,
-  updateUser
+  updateUser,
+  deleteUser
 } = require('../controllers/users');
 
 const router = express.Router();
@@ -12,11 +13,11 @@ const protect = require('../middleware/protect');
 const checkAdmin = require('../middleware/checkAdmin');
 const advancedResults = require('../middleware/query');
 
-router.route('/')
-  .get(protect, checkAdmin, getAllUsers)
+router.get('/', protect, checkAdmin, getAllUsers);
 
 router.route('/:id')
   .get(protect, checkAdmin, getSingleUser)
   .patch(protect, checkAdmin, updateUser)
+  .delete(protect, checkAdmin, deleteUser)
 
 module.exports = router;
